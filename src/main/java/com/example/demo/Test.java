6c36dc72447935729139e64c8247bf4b6c041a75
haritha <haritha.peryala@gmail.com>
2022-05-30 12:06:57 +0530

    added drl

---
+++ src/main/java/com/example/demo/Test.java
@@ -0,0 +1,21 @@
+package com.example.demo;
+
+public class Test {
+	
+	private String type;
+	public String getType() {
+		return type;
+	}
+	public void setType(String type) {
+		this.type = type;
+	}
+	public String getSla() {
+		return sla;
+	}
+	public void setSla(String sla) {
+		this.sla = sla;
+	}
+	private String sla;
+	
+
+}
